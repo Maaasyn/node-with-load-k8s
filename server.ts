def958a17eac0368ff0bb6747d7d7bc3a47469bb
maaasyn <wajdzk.martin@gmail.com>
2021-05-16 12:38:36 +0200

    prove that i use 2 pods to loadballance

--- server.ts
+++ server.ts
@@ -2,9 +2,14 @@ import express from "express";
 import helloRoutes from "./routes/hello";
 import cpuStressRoutes from "./routes/cpuStress";
 import ramStressRoutes from "./routes/ramStress";
+import { v4 as uuidv4 } from "uuid";
 import promBundle from "express-prom-bundle";
 // const metricsMiddleware = promBundle({ includeMethod: true });
 const app = express();
+
+const port = process.env.PORT || 3001;
+
+const uniqueIdentify = uuidv4();
 
 //Prometeus
 // app.use(metricsMiddleware);
@@ -15,9 +20,9 @@ app.use("/stress/ram", ramStressRoutes);
 app.use("/stress/cpu", cpuStressRoutes);
 
 app.get("/", (req, res, next) => {
-  res.send("Example app listening on port 3000!");
+  res.send(`App ${uniqueIdentify} is listening on port ${port}! `);
 });
 
-app.listen(3000, function () {
-  console.log("Example app listening on port 3000!");
+app.listen(port, () => {
+  console.log(`App ${uniqueIdentify} is listening on port ${port}! `);
 });
