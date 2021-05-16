ddfe4c4b9d098bb662fe93f24bc869179c86d3e4
maaasyn <wajdzk.martin@gmail.com>
2021-05-16 12:23:37 +0200

    changed service name to proper one

--- server.ts
+++ server.ts
@@ -3,11 +3,11 @@ import helloRoutes from "./routes/hello";
 import cpuStressRoutes from "./routes/cpuStress";
 import ramStressRoutes from "./routes/ramStress";
 import promBundle from "express-prom-bundle";
-const metricsMiddleware = promBundle({ includeMethod: true });
+// const metricsMiddleware = promBundle({ includeMethod: true });
 const app = express();
 
 //Prometeus
-app.use(metricsMiddleware);
+// app.use(metricsMiddleware);
 
 //Routes
 app.use("/hello", helloRoutes);
